5017f1a14840ed22b42a63d609b5f36a3a587381
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-28 08:52:13 +0900

    Refactor json2sqlite.pl and simplify

--- json2sqlite.pl
+++ json2sqlite.pl
@@ -17,26 +17,17 @@ my $TABLE = $OPT{t} || die $USAGE;
 
 my @CONTENT = <>;
 
-print STDERR "parsing JSON..\n";
+# print STDERR "parsing JSON..\n";
 my $DATA = decode_json(join("", @CONTENT));
 
 my $LEN = @{$DATA};
-print STDERR "output $LEN entries..\n";
+# print STDERR "output $LEN entries..\n";
 for (my $i=0; $i<$LEN; $i++) {
     my $entry = ${$DATA}[$i];
-    my @keys = keys %{$entry};
-
-    # print "$i\n";
-    # for my $key (@keys) {
-    #     print $key, "\t", $entry->{$key}, "\n";
-    # }
-
     my $id = $entry->{"id"};
     my $label = $entry->{"label"};
     my $value = $entry->{"value"};
     my $binId = $entry->{"binId"};
     my $binLabel = $entry->{"binLabel"};
-    # print "$i $label $value $binId $binLabel\n";
     print "insert into $TABLE (distribution, distribution_label, distribution_value, bin_id, bin_label) values ('$id', '$label', $value, '$binId', '$binLabel');\n";
-    # system "echo 'insert into $TABLE () values ($i $label $value $binId $binLabel)'";
 }
